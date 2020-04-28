0eb9d0bf691a6a3389a377254a2ebbf970df2c67
matthieutno <matthieu.tragno6@etu.univ-lorraine.fr>
2020-04-28 22:52:58 +0200

    Ajout de la classe MySQLCompteDAO avec l'interface correspondante

---
+++ bank/src/main/java/com/iut/as/daofactory/MySQLCompteDAO.java
@@ -0,0 +1,36 @@
+package com.iut.as.daofactory;
+
+import com.iut.as.interfaces.IDaoCompte;
+import com.iut.as.modele.Compte;
+
+public class MySQLCompteDAO implements IDaoCompte<Compte> {
+
+	private static MySQLCompteDAO instance;
+	
+	private MySQLCompteDAO() {}
+	
+	public static MySQLCompteDAO getInstance() {
+		if(instance == null) {
+			instance = new MySQLCompteDAO();
+		}
+		return instance;
+	}
+	
+	@Override
+	public void Create(Compte objet) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Update(Compte objet) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Delete(Compte objet) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ bank/src/main/java/com/iut/as/interfaces/IDaoCompte.java
@@ -0,0 +1,5 @@
+package com.iut.as.interfaces;
+
+public interface IDaoCompte<T> extends IDao<T>{
+
+}
